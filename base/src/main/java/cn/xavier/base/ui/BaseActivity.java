c932c72136c44dc19a9439dab7b1eb8f414cf9f9
杨晓辉 <yangxh108@126.com>
2017-07-28 12:22:18 +0800

    添加BaseActivity

---
+++ base/src/main/java/cn/xavier/base/ui/BaseActivity.java
@@ -0,0 +1,17 @@
+package cn.xavier.base.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Created by yangxh on 17/7/28.
+ */
+
+public class BaseActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
